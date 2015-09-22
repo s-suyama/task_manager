f3a27ca7384d2a37ccafdb4c7c671765fc69350b
shogo-suyama <suman.yarn@gmail.com>
2015-09-22 18:58:19 +0900

    users テーブルと projects テーブルの lock_version カラムのデフォルト値を0に変更しました。

--- db/migrate/20150921143046_create_users.rb
+++ db/migrate/20150921143046_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :mail_address
       t.string :password
-      t.integer :lock_version
+      t.integer :lock_version, default: 0
 
       t.timestamps null: false
     end
--- db/migrate/20150922084528_create_projects.rb
+++ db/migrate/20150922084528_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
-      t.integer :lock_version
+      t.integer :lock_version, default: 0
 
       t.timestamps null: false
     end
